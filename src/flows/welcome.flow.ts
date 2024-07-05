ab8698bbf0f6f8354f5101825cd2ba0fd7f01402
Aldo Eslyn Abril Ricalde <aldoabril@gmail.com>
2024-07-05 15:19:28 -0500

    fix:"Mejora presentacion"

--- src/flows/welcome.flow.ts
+++ src/flows/welcome.flow.ts
@@ -4,6 +4,5 @@ import mainLayer from "src/layers/main.layer";
 
 
 export const welcomeFlow = addKeyword(EVENTS.WELCOME)
-    .addAnswer(["Hola Boni, tu asistente virtual?"])
     .addAction(conversationalLayer)
     .addAction(mainLayer)
